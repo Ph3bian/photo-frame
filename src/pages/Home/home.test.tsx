2eaa4366d8a72fe0c7fe37b160cb66d6758dc86b
Ph3bian <phebianchukwurah@gmail.com>
2020-09-24 01:59:54 +0100

    folder structure and creating home view

--- src/pages/Home/home.test.tsx
+++ src/pages/Home/home.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Home from './home'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<Home />);
+  const linkElement = getByText(/photo frame/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from './home.module.scss';
+const Home = () => {
+  return <div className={styles.Home}>Photo Frame</div>;
+};
+export default Home;
